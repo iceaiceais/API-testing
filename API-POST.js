c3fb0e0a5d4516b4b1dde53c36b2b14c2cb305dc
iceaiceais <solehahnuraisyah@gmail.com>
2022-12-14 17:27:25 +0700

    API-POST

---
+++ API-POST.js
@@ -0,0 +1,10 @@
+it('Add a new user', () => {
+    var user = {
+        "name": 'Fathur Rohim',
+        "job": 'Test Engineer'
+    } 
+    cy.request('POST', 'https:reqres.in/api/users', user).then((response) => {
+        expect(response.status).equal(201)
+        expect({ name: 'Jane' }).to.eql({ name: 'Jane' })
+    })
+})
