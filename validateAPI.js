1d3cfd23b6f08feb1fb0ff6ef215f370e235013c
iceaiceais <118158206+iceaiceais@users.noreply.github.com>
2022-12-20 20:15:33 +0700

    validate content

--- validateAPI.js
+++ validateAPI.js
@@ -1,12 +1,26 @@
+
 /// <reference types="cypress" />
 
 it('validate respons body', ()=>{
     cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as("pokemon")
     cy.get("@pokemon").then((response)=>{
     
-        expect(response.body).to.have.property("name");
-
-       
+        expect(response.body).to.have.property("name"); 
     }) 
     })
+
+it('validate status code', ()=>{
+        cy.request("https://pokeapi.co/api/v2/pokemon/ditto").as("pokemon");
+        cy.get("@pokemon").its("status").should("eq", 200);
+})
+
+it.only('Validate Content', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
+        cy.get('@pokemon').then((response)=>{
     
+    expect(response.body.abilities[0].ability["name"]).to.eq("limber"); 
+        })  
+        
+});
+
+
